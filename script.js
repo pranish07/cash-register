984fbef1734b30bdd632363f06a8e477520afae9
pranish <pranishshrestha181@gmail.com>
2022-09-09 19:57:21 +0545

    style: responsivenes added

--- script.js
+++ script.js
@@ -3,8 +3,9 @@ const cashAmount = document.querySelector("#cash-amount");
 const nextButton = document.querySelector("#next-button");
 const checkButton = document.querySelector("#check-button");
 const errorMessage = document.querySelector(".error-message");
+const billErrorMessage = document.querySelector(".bill-error-message");
 const noOfNotes = document.querySelectorAll(".no-of-notes");
-const notes = [2000, 500, 50, 20, 10, 1];
+const notes = [2000, 500, 100, 20, 10, 5, 1];
 //open and close sections
 const openCheck = document.querySelector(".open-check");
 const openTable = document.querySelector(".open-table");
@@ -13,7 +14,14 @@ openTable.style.display = "none";
 openCheck.style.display = "none";
 
 nextButton.addEventListener("click", function updateBillAmount() {
-  openCheck.style.display = "block";
+  openCheck.style.display = "none";
+  if (billAmount.value > 0) {
+    openCheck.style.display = "block";
+    billErrorMessage.innerText = null;
+  } else {
+    billErrorMessage.innerText = `Please make bill greater than zero.`;
+    openCheck.style.display = "none";
+  }
 });
 checkButton.addEventListener("click", function updateBillAmountAndCashAmount() {
   openTable.style.display = "none";
@@ -21,22 +29,20 @@ checkButton.addEventListener("click", function updateBillAmountAndCashAmount() {
     // displayNone();
     openTable.style.display = "block";
     if (Number(cashAmount.value) >= Number(billAmount.value)) {
-      errorMessage.innerText=`cash Amount is ${cashAmount.value} and Bill amount is ${ billAmount.value}`;
-      
+      errorMessage.innerText = `cash Amount is ${cashAmount.value} and Bill amount is ${billAmount.value}`;
+
       let amountToBeReturned = cashAmount.value - billAmount.value;
-     
+
       calculateChange(amountToBeReturned);
-     
     } else {
       displayBlock();
       message("Give more money");
-      openTable.style.display ="none";
+      openTable.style.display = "none";
     }
   } else {
     displayBlock();
     message("Please enter an amount greater than 0");
   }
-  
 });
 
 function calculateChange(amountToBeReturned) {
@@ -56,5 +62,5 @@ function displayNone() {
   errorMessage.style.display = "none";
 }
 function displayBlock() {
-    errorMessage.style.display = "block";
+  errorMessage.style.display = "block";
 }
